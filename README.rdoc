3a5e2edfe72dba518cc13a7f64272663a5813d59
danish <danish@danish-ubuntu.(none)>
2013-02-02 00:21:44 -0500

    Initial commit

---
+++ README.rdoc
@@ -0,0 +1,4 @@
+== LunchMatch
+
+Helps you find new people to have lunch with according to where you are, your schedule, and your interests.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchmatch::Application.config.secret_token = 'ad78df1794da8bfc69db7634d2c93f4319d42d308ebdb67bec8416fd2558b9290149f86166e3c70a9a901454d5a72c272795714a8e8b493525ca5880de486399'
